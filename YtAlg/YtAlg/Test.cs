a134ba71e90f2606fe8b2ca8cea38cf4d88cec19
Alex Woodley <eartlsaki@gmail.com>
2018-05-08 19:50:03 +0100

    Added Packages Cut Firefox install

---
+++ YtAlg/YtAlg/Test.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YtAlg
+{
+    class Test
+    {
+        IWebDriver driver;
+
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new FirefoxDriver("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://www.youtube.com/";
+        }
+
+        [TearDown]
+
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
